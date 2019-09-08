de0b097bc5ffeab268a5a0a9dee47e186972344f
PAUL VIRREY <paul.v.m.virrey@accenture.com>
2019-09-08 21:23:30 +0800

    implemented subscription plan

--- controllers/paymentsController.js
+++ controllers/paymentsController.js
@@ -1,19 +1,47 @@
 const stripe = require("stripe")("sk_test_7eXVixhBDdgwXyRA50MrY8yk00akjFbrDL");
+const user_plan = "plan_FlwPmsQn894kG9";
+const vendor_plan = "plan_FlwQEuaOUm4MRQ";
 
 function paymentsController() {
   async function charge(req, res) {
+    let cust = "";
     try {
-      let { status } = await stripe.charges.create({
-        amount: 2000,
-        currency: "usd",
-        description: "An example charge",
+      cust = await stripe.customers.create({
+        email: req.body.email,
+        name: req.body.name,
         source: req.body.token
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    try {
+      const current_plan =
+        req.body.subscription_type == "user" ? user_plan : vendor_plan;
+      let { status } = await stripe.subscriptions.create({
+        customer: cust.id,
+        items: [
+          {
+            plan: current_plan
+          }
+        ]
       });
 
       res.json({ status });
     } catch (err) {
       res.status(500).send(err);
     }
+
+    //   // let { status } = await stripe.charges.create({
+    //   //   amount: 2000,
+    //   //   currency: "usd",
+    //   //   description: "An example charge",
+    //   //   source: req.body.token
+    //   // });
+
+    //   res.json({ status });
+    // } catch (err) {
+    //   res.status(500).send(err);
+    // }
   }
   return { charge };
 }
